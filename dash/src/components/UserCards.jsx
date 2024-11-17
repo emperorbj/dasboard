a68a9260e0d6ad5a14aeef158eadebdebe29aa15
emperorbj <cwaku96@gmail.com>
2024-11-17 19:46:19 +0100

    "Updated styling and spacing in UserCard component"

--- dash/src/components/UserCards.jsx
+++ dash/src/components/UserCards.jsx
@@ -27,7 +27,7 @@ const UserCard = ({ item }) => {
                 <Card className="w-[370px] hover:bg-sky-100 hover:shadow-xl hover:shadow-sky-100 sm:w-[280px] 
                 flex flex-col justify-center items-center md:w-[270px] lg:w-[320px]">
                     <CardHeader>
-                        <CardTitle className="dark:text-blue-500 flex items-center gap-6 mb-5">
+                        <CardTitle className="dark:text-blue-500 flex items-center gap-6 mb-3">
                                 <div>
                                     <Avatar>
                                         <AvatarImage src={item?.img} alt="@shadcn" />
@@ -40,15 +40,15 @@ const UserCard = ({ item }) => {
                                     <h1 className="font-bold md:text-lg lg:text-xl text-sky-600">
                                     {item?.fullName}
                                     </h1>
-                                    <h2 className="font-normal md:text-[12px] lg:text-lg">
+                                    <h2 className="font-normal md:text-[12px] lg:text-[13px]">
                                     {item?.job}
                                     </h2>
-                                    <h2 className="font-normal md:text-[12px] lg:text-lg">
+                                    <h2 className="font-normal md:text-[12px] lg:text-[13px]">
                                         {item?.email}
                                     </h2>
                                 </div>
                             </CardTitle>
-                        <CardDescription className='md:text-[12px] lg:text-lg'> 
+                        <CardDescription className='md:text-[12px] lg:text-[13px]'> 
                             {item?.shortbio}
                         </CardDescription>
                     </CardHeader>
@@ -61,7 +61,7 @@ const UserCard = ({ item }) => {
                                 {item?.weight}
                             </h1>
                         </div>
-                        <div className="flex flex-col gap-2">
+                        <div className="flex flex-col gap-1">
                             <h2 className="text-sky-600 font-semibold">
                                 Height
                             </h2>
@@ -69,7 +69,7 @@ const UserCard = ({ item }) => {
                                 {item?.height}
                             </h1>
                         </div>
-                        <div className="flex flex-col gap-2">
+                        <div className="flex flex-col gap-1">
                             <h2 className="text-sky-600 font-semibold">
                                 Age
                             </h2>
