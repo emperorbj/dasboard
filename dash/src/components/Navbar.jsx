93e784444b163cb3a0f11bf45c7337c6632d5a20
emperorbj <cwaku96@gmail.com>
2024-11-18 05:01:10 +0100

    "Update Navbar title from 'ULife' to 'WeCare'"

--- dash/src/components/Navbar.jsx
+++ dash/src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
                 <div className='shadow-md h-[35px] w-[35px] rounded-md flex items-center justify-center bg-sky-400'>
                     <LuHeartHandshake className='text-white h-[26px] w-[26px]'/>
                 </div>
-                <h1 className='text-sky-400 font-bold text-xl'><span className='text-green-500'>U</span>Life</h1>
+                <h1 className='text-sky-400 font-bold text-xl'><span className='text-green-500'>We</span>Care</h1>
             </div>
             <div className="flex items-center gap-3">
 
